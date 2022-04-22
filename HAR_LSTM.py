45f242ded1d9c2d5261caeaac17fc328b4cbe742
ZZD <1051439873@qq.com>
2022-04-22 09:24:31 +0800

    2022.4.22 训练次数1000次后loss降低但是正确率还是40上下 接下来尝试二分类网络 增加一个例程

--- HAR_LSTM.py
+++ HAR_LSTM.py
@@ -18,13 +18,13 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 
 
-TRAIN_PATH='D:\Graduate Program\Train Data\KTH\Train_data'
-TEST_PATH='D:\Graduate Program\Train Data\KTH\Test_data'
-MODEL_PATH='D:\Graduate Program\Train Data\KTH\Model'
+TRAIN_PATH = 'D:\Graduate Program\Train Data\KTH\Train_data'
+TEST_PATH = 'D:\Graduate Program\Train Data\KTH\Test_data'
+MODEL_PATH = 'D:\Graduate Program\Train Data\KTH\Model'
 
-MAX_FRAMENUM=150
-VIDEO_WIDTH=160
-VIDEO_HEIGHT=120
+MAX_FRAMENUM = 50
+VIDEO_WIDTH = 160
+VIDEO_HEIGHT = 120
 
 
 train_on_gpu = torch.cuda.is_available()
@@ -43,26 +43,32 @@ def count_sample(path):
     return count
 
 
-def load_dataset(sample_num,path):
+def load_dataset(sample_num, path):
 
     files = os.listdir(path)
 
     y = np.zeros((sample_num, 1), dtype=np.float32)
     x = np.zeros((sample_num, MAX_FRAMENUM, 1, VIDEO_HEIGHT, VIDEO_WIDTH), dtype=np.float32) #存储视频用的numpy数组
 
-    cnt_file=0
+    cnt_file = 0
     int(cnt_file)
 
     for file in files:
         videopath = path+"/"+file
         cap=cv.VideoCapture(videopath)
 
-        cnt=0
+        cnt = 0
         while(cap.isOpened() and cnt<MAX_FRAMENUM):
-            a , b = cap.read()
+            a, b = cap.read()
+            if a ==False: break
             gray = cv.cvtColor(b, cv.COLOR_BGR2GRAY)/255    #将3通道的RGB图像转换为灰度图像
             x[cnt_file][cnt][0] = gray
             cnt += 1
+            for i in range(3):
+                a, b = cap.read()
+                if a == False: break
+        cap.release()
+
 
         if 'boxing' in file:
             y[cnt_file] = 0.2
@@ -78,7 +84,6 @@ def load_dataset(sample_num,path):
             y[cnt_file] = 0.7
 
         cnt_file += 1
-
     # transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     # dataloader_x = torch.utils.data.DataLoader(x, batch_size=sample_num, shuffle=False, num_workers=2)
 
@@ -95,7 +100,7 @@ class HARModel(nn.Module):
         self.conv = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(3, 4), stride=(3, 4))
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.lstm = nn.LSTM(input_size=400, hidden_size=100, num_layers=n_layers, batch_first = True)
+        self.lstm = nn.LSTM(input_size=400, hidden_size=100, num_layers=n_layers, batch_first=True)
 
         self.fc1 = nn.Linear(100, 50)
         self.fc2 = nn.Linear(50, 16)
@@ -105,24 +110,19 @@ class HARModel(nn.Module):
 
     def forward(self, x):
 
-        # for i in range(MAX_FRAMENUM-1):
-        #
-        #    x[i] = Variable(x[i])
-        #    x[i] = self.conv(x[i])
-        #    x[i] = self.pool(x[i])
         x = self.conv(x)
         x = self.pool(x)
 
         #batch_first=True时，lstm的imput格式为（batch_size,len,channels)
-        x = x.view(1, 150, 400)         #(batch_size, len ,channel)
+        x = x.view(1, MAX_FRAMENUM, 400)         #(batch_size, len ,channel)
         x, (h_s, h_c) = self.lstm(x)
 
-        x = torch.tanh(self.fc1(x))
-        x = torch.tanh(self.fc2(x))
-        x = torch.tanh(self.fc3(x))
+        output = torch.sigmoid(self.fc1(h_c))
+        output = torch.sigmoid(self.fc2(output))
+        output = torch.relu(self.fc3(output))
 
-        x = x.view(1, 150)
-        output = x[0][149]
+        output = torch.squeeze(output)
+
 
         return output
 
@@ -133,10 +133,10 @@ if __name__ == '__main__':
 
     #load the train data and the test data
     print("loading train data.......")
-    x_train,y_train=load_dataset(count_sample(TRAIN_PATH),TRAIN_PATH)
+    x_train,y_train=load_dataset(count_sample(TRAIN_PATH), TRAIN_PATH)
     print("train data load success!")
     print("loading test data.......")
-    x_test,y_test=load_dataset(count_sample(TEST_PATH),TEST_PATH)
+    x_test,y_test=load_dataset(count_sample(TEST_PATH), TEST_PATH)
     print("test data load success!")
 
     #create the LSTM network
@@ -150,7 +150,7 @@ if __name__ == '__main__':
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
     #start training the network
-    for epoch in range(200):
+    for epoch in range(1000):
         running_loss = 0.0
         for i, data in enumerate(x_train):
             inputs = data
@@ -159,32 +159,26 @@ if __name__ == '__main__':
             #inputs, labels = Variable(inputs), Variable(labels)
             #inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
 
-            inputs, labels =inputs.type(torch.FloatTensor), labels.type(torch.FloatTensor)
+            inputs, labels = inputs.type(torch.FloatTensor), labels.type(torch.FloatTensor)
             inputs, labels = inputs.to(device), labels.to(device)
 
             optimizer.zero_grad()
-            # forward + backward + optimizer
             outputs = model(inputs)
             outputs = outputs.type(torch.FloatTensor)
-
 
             outputs = torch.unsqueeze(outputs, 0)
             outputs = torch.unsqueeze(outputs, 0)
             labels = torch.unsqueeze(labels, 0)
             outputs = outputs.to(device)
 
-            # print(outputs)
-            # print(labels)
-
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
-            #print(loss.item)
 
             running_loss += loss.item()
             if i % 200 == 199:
-                print('[%d , %5d] loss: %.20f' % (epoch + 1, i + 1, running_loss/200))
+                print('[%d , %5d] loss: %.5f' % (epoch + 1, i + 1, running_loss/200))
                 running_loss = 0.0
 
         if (epoch+1) % 5 == 0:
@@ -206,7 +200,7 @@ if __name__ == '__main__':
                 # else:
                 #     print(y_test[i])
             print('total num: %d, correct_num: %d' % (test_num_total, test_num_correct))
-            print('Accuracy: %.5f %%' % (test_num_correct*100 / test_num_total))
+            print('Accuracy: %.3f %%' % (test_num_correct*100 / test_num_total))
 
     # torch.save(model, MODEL_PATH)
     # print("model save success!")
